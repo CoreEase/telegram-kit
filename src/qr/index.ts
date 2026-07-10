2c540fc1b1e24331af3713e734c8468823f02f8f
Core Ease <mr.elsoghiar@gmail.com>
2026-07-10 14:05:53 +0300

    Update index.ts

--- src/qr/index.ts
+++ src/qr/index.ts
@@ -1,2 +1,5 @@
 export { QRCode } from './QrCode';
 export type { QRCodeProps } from './QrCode';
+
+export { encodeQRCode, qrCodeToSVG } from './Qr';
+export type { QrEncodeOptions, QrEncodeResult, ErrorCorrectionLevel, QrSvgOptions } from './Qr';
