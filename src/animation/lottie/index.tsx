7c63bf6d9c9cd83cdacfb8f7fe6e76c2c86db590
Core Ease <mr.elsoghiar@gmail.com>
2026-07-10 04:10:53 +0300

    Create index.tsx

---
+++ src/animation/lottie/index.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import {
+  createAnimationPlayerComponent,
+  type AnimationPlayerHandle,
+  type AnimationPlayerBaseProps,
+} from '../../_internal/animation/AnimationPlayerBase';
+import { loadTgsSource } from '../../_internal/animation/lottie/loader';
+
+export type { TgsSource as LottieSource } from '../../_internal/animation/lottie/loader';
+export type { LottieAnimation } from '../../_internal/animation/lottie/types';
+export type { PlayMode, PlayerState } from '../../_internal/animation/lottie/animation';
+
+export type LottiePlayerHandle = AnimationPlayerHandle;
+
+export interface LottiePlayerProps extends Omit<AnimationPlayerBaseProps, 'validate'> {}
+
+const BaseAnimationPlayer = createAnimationPlayerComponent('LottiePlayer', 'Lottie animation');
+
+export const LottiePlayer = React.forwardRef<LottiePlayerHandle, LottiePlayerProps>(
+  function LottiePlayer(props, ref) {
+    return <BaseAnimationPlayer {...props} ref={ref} />;
+  }
+);
+
+LottiePlayer.displayName = 'LottiePlayer';
+
+export { loadTgsSource as loadLottieSource };
+
+export { LottieAnimationController } from '../../_internal/animation/lottie/animation';
+export { renderDocumentFrame } from '../../_internal/animation/lottie/engine';
